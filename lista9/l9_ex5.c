272505e0dc4984abb93f4187e858080c643e4462
romildodcm <romildodcm@gmail.com>
2021-10-27 22:31:02 -0300

    até o 6

--- lista9/l9_ex5.c
+++ lista9/l9_ex5.c
@@ -1,8 +1,46 @@
 #include <stdio.h>
-#include <math.h>
 
 int main()
 {
+    int i = 0, j = 0, j2 = 0, n = 12, sum = 0;
+    char t;
+    double m[n][n], resultado = 0.0;
+
+    scanf(" %c", &t);
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            scanf(" %lf", &m[i][j]);
+        }
+    }
+
+    // // DEBUG
+    // printf("-------------------- \n");
+    // Acima da Diagonal Secundária
+    j2 = n - 1; //10
+    for (i = 0; i < n-1; i++)
+    {
+        for (j = 0; j < j2; j++)
+        {
+            resultado = resultado + m[i][j];
+            sum++;
+            // // DEBUG
+            // printf(" %.1lf", m[i][j]);
+        }
+        j2--;
+
+        // // DEBUG
+        // printf(" \n");
+    }
+    // // DEBUG
+    // printf("-------------------- \n");
+
+    if (t == 'M')
+        resultado = resultado / sum;
+
+    printf("%.1lf\n", resultado);
 
     return 0;
 }
--- lista9/l9_ex6.c
+++ lista9/l9_ex6.c
@@ -1,8 +1,47 @@
 #include <stdio.h>
-#include <math.h>
 
 int main()
 {
+    int i = 0, j = 0, j2 = 0, j_move = 0, n = 12, sum = 0;
+    char t;
+    double m[n][n], resultado = 0.0;
+
+    scanf(" %c", &t);
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            scanf(" %lf", &m[i][j]);
+        }
+    }
+
+    // // DEBUG
+    // printf("-------------------- \n");
+    // Acima da Diagonal Secundária
+    j2 = n - 2; //10
+    j_move = n-1; //11
+    for (i = 1; i < n; i++)
+    {
+        for (j = j_move; j > j2; j--)
+        {
+            resultado = resultado + m[i][j];
+            sum++;
+            // // DEBUG
+            // printf(" %.1lf", m[i][j]);
+        }
+        j2--;
+
+        // // DEBUG
+        // printf(" \n");
+    }
+    // // DEBUG
+    // printf("-------------------- \n");
+
+    if (t == 'M')
+        resultado = resultado / sum;
+
+    printf("%.1lf\n", resultado);
 
     return 0;
 }
